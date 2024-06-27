e2624b74caa1733eb1357e0a5c7d7de6c2de9e6a
Sergey Yedovin <edovin.sergey@gmail.com>
2024-06-27 21:55:44 +0300

    heggeleggleggoo

---
+++ 7 kyu/heggeleggleggo.py
@@ -0,0 +1,26 @@
+```
+Egg Talk.
+
+Insert an "egg" after each consonant. If there are no consonants, there will be no eggs. Argument will consist of a string with only alphabetic characters and possibly some spaces.
+
+eg:
+
+hello => heggeleggleggo
+
+eggs => egegggeggsegg
+
+FUN KATA => FeggUNegg KeggATeggA
+
+////
+
+This Kata is designed for beginners to practice the basics of regular expressions. With this in mind a little bit of commenting in your solution could be very useful.
+
+Check Eloquent Javascript p176
+
+https://regex101.com/#javascript
+
+
+```
+
+def heggeleggleggo(word: str) -> str:
+    return ''.join([i+'egg' if i not in 'aeiouAEIOU ' else i for i in word])
