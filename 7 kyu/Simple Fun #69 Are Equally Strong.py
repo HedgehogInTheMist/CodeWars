a7b9de48e82e0e84bdd7bae15d10ae8f8be729f9
Sergey Yedovin <edovin.sergey@gmail.com>
2024-06-28 22:35:22 +0300

    Simple Fun #69 Are Equally Strong

---
+++ 7 kyu/Simple Fun #69 Are Equally Strong.py
@@ -0,0 +1,41 @@
+```
+Task
+Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+
+Call two people equally strong if their strongest arms are equally strong (the strongest arm can be both the right and the left), and so are their weakest arms.
+
+Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
+
+Example
+For yourLeft = 10, yourRight = 15, friendsLeft = 15 and friendsRight = 10, the output should be true;
+
+For yourLeft = 15, yourRight = 10, friendsLeft = 15 and friendsRight = 10, the output should be true;
+
+For yourLeft = 15, yourRight = 10, friendsLeft = 15 and friendsRight = 9, the output should be false.
+
+Input/Output
+[input] integer yourLeft
+
+A non-negative integer representing the heaviest weight you can lift with your left arm.
+
+[input] integer yourRight
+
+A non-negative integer representing the heaviest weight you can lift with your right arm.
+
+[input] integer friendsLeft
+
+A non-negative integer representing the heaviest weight your friend can lift with his or her left arm.
+
+[input] integer friendsRight
+
+A non-negative integer representing the heaviest weight your friend can lift with his or her right arm.
+
+[output] a boolean value
+```
+
+def are_equally_strong(your_left: int, your_right: int, friends_left: int, friends_right: int):
+    if (your_left == friends_left or your_left == friends_right) or (your_right == friends_right or your_right == friends_left):
+        return your_left + your_right == friends_left + friends_right
+    else:
+        return False
+    
