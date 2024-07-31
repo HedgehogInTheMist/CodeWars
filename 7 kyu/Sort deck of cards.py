60b654b178a24a0262268451b9ecf5afd0bf94a9
Sergey Yedovin <edovin.sergey@gmail.com>
2024-07-31 22:53:16 +0300

    Sort deck of cards

---
+++ 7 kyu/Sort deck of cards.py
@@ -0,0 +1,21 @@
+```
+Write a function sort_cards() that sorts a shuffled list of cards, so that any given list of cards is
+sorted by rank, no matter the starting collection.
+
+All cards in the list are represented as strings, so that sorted list of cards looks like this:
+
+['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
+
+Example:
+
+>>> sort_cards(['3', '9', 'A', '5', 'T', '8', '2', '4', 'Q', '7', 'J', '6', 'K'])
+['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
+Hint: Tests will have many occurrences of same rank cards, as well as vary in length. 
+You can assume though, that input list is always going to have at least 1 element.
+```
+
+def sort_cards(cards):
+    deck = {
+        'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13        
+    }
+    return sorted(cards, key=lambda cards: deck[cards])
