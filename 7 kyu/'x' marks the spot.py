1f3fe129ed3a6cfec5746c6e3c2944673eb19943
Sergey Yedovin <edovin.sergey@gmail.com>
2024-10-24 22:04:28 +0300

    x marks the spot

---
+++ 7 kyu/'x' marks the spot.py
@@ -0,0 +1,44 @@
+```
+Given a two dimensional array, return the co-ordinates of x.
+
+If x is not inside the array, or if x appears multiple times, return [].
+
+The co-ordinates should be zero indexed in row-major order.
+You should assume you will always get an array as input. The array will only contain 'x's and 'o's.
+
+Examples
+Input: []
+Return an empty array if input is an empty array => []
+
+Input: [
+  ['o', 'o'],
+  ['o', 'o']
+]
+Return an empty array if no x found => []
+
+Input: [
+  ['x', 'o'],
+  ['o', 'x']
+]
+Return an empty array if more than one x found => []
+
+Input: [
+  ['x', 'o'],
+  ['o', 'o']
+]
+Return [0,0] when x at top left => [0, 0]
+
+Input: [
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'x', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
+]
+Return [4,6] for the example above => [4, 6]
+```
+
+def x_marks_the_spot(mat: list) -> list:
+    return next(([i, j] for i, sublist in enumerate(mat) for j, item in enumerate(sublist) if item == 'x' and sum(item == 'x' for sublist in mat for item in sublist) == 1), [])
+    
