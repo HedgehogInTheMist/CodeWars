a7de20ac81b8570c4024c6c41b5eacb1c9902c32
Sergey Yedovin <edovin.sergey@gmail.com>
2025-03-17 22:18:23 +0300

    Counting Characters

---
+++ 8 kyu/Counting Characters.py
@@ -0,0 +1,6 @@
+```
+Define a method named countCharOccurrences or count_char_occurrences that accepts a string and a char as inputs
+and returns the number of times the char occurs in the string as an int.
+```
+def count_char_occurrences(strng: str, char: str) -> int:
+    return strng.count(char)
