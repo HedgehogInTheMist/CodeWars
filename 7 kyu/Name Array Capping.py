ff541ee47116633306390a3d5840f1b16b27edaf
Sergey Yedovin <edovin.sergey@gmail.com>
2025-05-15 23:04:19 +0300

    Name Array Capping

---
+++ 7 kyu/Name Array Capping.py
@@ -0,0 +1,10 @@
+```
+Create a function that accepts an array of names, and returns an array of each name with its first letter capitalized and the remainder in lowercase.
+
+Examples
+['jo', 'nelson', 'jurie'] -->  ['Jo', 'Nelson', 'Jurie']
+['KARLY', 'DANIEL', 'KELSEY'] --> ['Karly', 'Daniel', 'Kelsey']
+```
+
+def cap_me(arr):
+    return [i.capitalize() for i in arr]
